d5112cc94e00aec38d1186bc64f5985e08084f82
Jonas Jostan <jonas.jostan@iteratec.com>
2022-07-22 10:49:17 +0200

    Semantics hinzugefügt

--- lib/src/widget/default_show_hide_password_button.dart
+++ lib/src/widget/default_show_hide_password_button.dart
@@ -19,30 +19,32 @@ class DefaultShowHidePasswordButton extends StatelessWidget {
   final IconData? _showPasswordIcon;
   final IconData? _hidePasswordIcon;
   final Function() _onPressed;
-  static const String _showPasswordDescription = "Zeigt das Password im Klartext";
+  static const String _showPasswordDescription =
+      "Zeigt das Password im Klartext";
   static const String _hidePasswordDescription = "Versteckt das Passwort";
-
 
   @override
   Widget build(BuildContext context) {
-    return buildWidgetWithSemantics(_hidePassword?_showPasswordDescription:_hidePasswordDescription,  Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        GestureDetector(
-          onTap: _onPressed,
-          child: _hidePassword
-              ? FaIcon(
-                  _hidePasswordIcon ?? FontAwesomeIcons.eye,
-                )
-              : Padding(
-                  padding: const EdgeInsets.only(right: 6),
-                  child: Icon(
-                    _showPasswordIcon ?? FontAwesomeIcons.eyeSlash,
-                  ),
-                ),
-        ),
-      ],
-    ));
+    return buildWidgetWithSemantics(
+        _hidePassword ? _showPasswordDescription : _hidePasswordDescription,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: _onPressed,
+              child: _hidePassword
+                  ? FaIcon(
+                      _hidePasswordIcon ?? FontAwesomeIcons.eye,
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.only(right: 6),
+                      child: Icon(
+                        _showPasswordIcon ?? FontAwesomeIcons.eyeSlash,
+                      ),
+                    ),
+            ),
+          ],
+        ));
   }
 }
